792f8afe3de554047c02fbdc9ca4f5638d9ad72e
fllipeis <p.eistrach@gmail.com>
2025-08-03 21:04:56 +0200

    feat: 1.21.8 support

--- command-velocity/build.gradle.kts
+++ command-velocity/build.gradle.kts
@@ -32,6 +32,7 @@ modrinth {
         "1.21.5",
         "1.21.6",
         "1.21.7",
+        "1.21.8",
     )
     loaders.add("velocity")
     changelog.set("https://docs.simplecloud.app/changelog")
