9a1bb0c5e796d9d367c0754f754809d23603e89a
fllipeis <p.eistrach@gmail.com>
2026-09-16 12:55:01 +0200

    chore: add Minecraft 26.3 to supported versions and bump release

--- command-bungeecord/build.gradle.kts
+++ command-bungeecord/build.gradle.kts
@@ -39,6 +39,7 @@ modrinth {
         "26.1.1",
         "26.1.2",
         "26.2",
+        "26.3",
     )
     loaders.addAll("bungeecord", "waterfall")
     changelog.set("https://docs.simplecloud.app/changelog")
